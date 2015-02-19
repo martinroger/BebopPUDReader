96d74b0030198c3b844b9c7a5cfd72c4e3c84e19
Martin ROGER <martinroger@pc29.home>
2015-02-19 18:19:05 +0100

    binaryRaw class methods have been completed

--- Bebop PUD Reader/ViewController.swift
+++ Bebop PUD Reader/ViewController.swift
@@ -9,6 +9,9 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    var jsonUsefulInfos = [ String : String ]() //Dictionary linked to fun extractUsefulJSONData
+    var hashedData = [dataLine]()               //array of dataLines is final product that needs to be typecase to String for CSV
 
     @IBOutlet weak var currentStatus: NSTextField!
     @IBOutlet weak var flightDateToDisplay: NSTextField!
@@ -31,23 +34,51 @@ class ViewController: NSViewController {
     }
 
     @IBAction func loadPUDFilePressed(sender: NSButton) {
+        var rawData = binaryRAW() //This is where all the data will be handled in raw form
+        
+        self.currentStatus.stringValue = "Opening PUD File"
+        rawData.binaryRaw = self.openPUDFile()
+        rawData.scindRawData(rawData.findEndOfJSON())
+        self.currentStatus.stringValue = "JSON end found"
+        jsonUsefulInfos = self.extractUsefulJSONData(rawData.jsonData)
+        self.currentStatus.stringValue = "JSON header parsed"
+        
+        self.currentStatus.stringValue = "RAW data parsed"
+        updateDisplay()
+        self.currentStatus.stringValue = "Infos updated"
+        
+        self.currentStatus.stringValue = "Ready to save"
     }
     @IBAction func saveKMLFilePressed(sender: NSButton) {
     }
     @IBAction func saveCSVFilePressed(sender: NSButton) {
+        self.currentStatus.stringValue = "Saving..."
+        saveCSVFile()
+        self.currentStatus.stringValue = "CSV File Saved"
     }
 
     func updateDisplay() {
         
     }
     
-    func openPUDFile() {
+    func openPUDFile() -> NSData {
+        var raw = NSData()
+        //This should contain the NSOpenPanel sequence
         
+        return raw
     }
     
     func saveCSVFile() {
         
     }
+    
+    func extractUsefulJSONData(JSONData: NSData) -> [ String : String ] {
+        var infos = [ String : String ]()
+        //somewhere in here use NSJSONSerialization
+        
+        return infos
+    }
+    
     
 }
 
--- BinaryRAW.swift
+++ BinaryRAW.swift
@@ -13,14 +13,29 @@ class binaryRAW {
     var jsonData = NSData()
     var flightData = NSData()
     
-    func findEndOfJSON(data: NSData) -> Int {
+    func findEndOfJSON() -> Int {
         var endOfJSONIndex: Int = 0
-        
+        var openCurly: Int = 0
+        var closedCurly: Int = 0
+        while(true) {
+            if NSString(data: self.binaryRaw.subdataWithRange(NSMakeRange(endOfJSONIndex , 1)), encoding: NSUTF8StringEncoding)! == "{" {
+                openCurly++
+            }
+            if NSString(data: self.binaryRaw.subdataWithRange(NSMakeRange(endOfJSONIndex , 1)), encoding: NSUTF8StringEncoding)! == "}" {
+                closedCurly++
+            }
+            endOfJSONIndex++
+            //breakout condition
+            if openCurly == closedCurly {
+                break
+            }
+        }
         return endOfJSONIndex
     }
     
     func scindRawData(index: Int) {
-        
+        self.jsonData = self.binaryRaw.subdataWithRange(NSMakeRange(0, index))
+        self.flightData = self.binaryRaw.subdataWithRange(NSMakeRange(index , binaryRaw.length - index )) //gros doute
     }
     
 }
