16060842df58f04c90dd4159aa7fdd28de403ba3
Martin ROGER <martinroger@pc29.home>
2015-02-19 15:52:43 +0100

    IBOutlets et IBActions en place

--- Bebop PUD Reader/ViewController.swift
+++ Bebop PUD Reader/ViewController.swift
@@ -10,7 +10,13 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var currentStatus: NSTextField!
     @IBOutlet weak var flightDateToDisplay: NSTextField!
+    @IBOutlet weak var serialNumberToDisplay: NSTextField!
+    @IBOutlet weak var uuidToDisplay: NSTextField!
+    @IBOutlet weak var flightDurationToDisplay: NSTextField!
+    @IBOutlet weak var maxHeightToDisplay: NSTextField!
+    @IBOutlet weak var maxSpeedToDisplay: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +30,12 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func loadPUDFilePressed(sender: NSButton) {
+    }
+    @IBAction func saveKMLFilePressed(sender: NSButton) {
+    }
+    @IBAction func saveCSVFilePressed(sender: NSButton) {
+    }
 
 }
 
